7c3288cf1b40bab706eec3b4995f6e5940dc9def
Daniel Mills <evilhamsterman@users.noreply.github.com>
2022-04-12 14:25:03 -0700

    make BW_SESSION universal

--- functions/bwunlock.fish
+++ functions/bwunlock.fish
@@ -1,7 +1,7 @@
 function bwunlock -d "Unlock and set the Bitwarden session"
     set bwsession (command bw unlock --raw $argv)
     if test $status = 0
-        set -gx BW_SESSION $bwsession
+        set -Ux BW_SESSION $bwsession
     else
         return 1
     end
