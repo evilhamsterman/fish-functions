1723022f126935722f1b40147a95f4a0b4fc6294
Dan Mills <evilhamsterman@gmail.com>
2022-04-14 13:56:56 -0700

    Remove mkvenv

--- functions/mkvenv.fish
+++
@@ -1,9 +0,0 @@
-function mkvenv
-    if count $argv > /dev/null
-        set venv $argv
-    else
-        set venv .venv
-    end
-    command python3 -m venv $venv
-    . $venv/bin/activate.fish
-end
